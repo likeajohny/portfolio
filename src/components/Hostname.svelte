f897378abae1893a8fc2f13e0bfd4f060a865830
likeajohny <likeajohny@users.noreply.github.com>
2025-10-05 20:59:47 +0200

    Implement Hostname component

---
+++ src/components/Hostname.svelte
@@ -0,0 +1,18 @@
+<script>
+    let hostname = "";
+    if (typeof window !== "undefined") {
+        hostname = window.location.hostname;
+    }
+</script>
+
+<span class="text-gradient">{hostname}</span>
+
+<style>
+    .text-gradient {
+		background-image: var(--accent-gradient);
+		-webkit-background-clip: text;
+		-webkit-text-fill-color: transparent;
+		background-size: 400%;
+		background-position: 0;
+	}
+</style>
